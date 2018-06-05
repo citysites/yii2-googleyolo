f1162fac923cfef11c0d4ac558e128ec51c726e9
Ivan Aslanov <bmw_x591@mail.ru>
2018-06-05 16:46:40 +0300

    Fix asset

--- GoogleYoloAsset.php
+++ GoogleYoloAsset.php
@@ -4,10 +4,17 @@ namespace bmwx591\googleyolo;
 
 
 use yii\web\AssetBundle;
+use yii\web\View;
 
 class GoogleYoloAsset extends AssetBundle
 {
     public $sourcePath = '@vendor/bmwx591/yii2-googleyolo';
+
+    public $publishOptions = [];
+
+    public $jsOptions = [
+        'position' => View::POS_HEAD
+    ];
 
     public $js = [
         'https://smartlock.google.com/client',
