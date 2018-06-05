387680677f74765d67ebf45d115b98c3ff848471
Ivan Aslanov <bmw_x591@mail.ru>
2018-06-06 01:24:47 +0300

    Add observer to js

--- js/yolo.js
+++ js/yolo.js
@@ -35,5 +35,30 @@ const GoogleYolo = function (options) {
         } else {
             hintPromise();
         }
+
+        // GoogleYolo doesn't insert their iframe immediately. This mutation
+        // observer watches the <body> to tell when they insert the <iframe> so
+        // we can style as needed.
+        const bodyObserver = new MutationObserver(function(mutationsList) {
+            mutationsList.forEach(function(mutation) {
+                mutation.addedNodes.forEach(function(node) {
+                    if (node.nodeName === 'IFRAME' && node.src.includes('smartlock.google.com/iframe/')) {
+                        bodyObserver.disconnect();
+                        node.classList.add('google-inserted-frame');
+                        // We need one more observer to watch the <iframe> that Google Yolo
+                        // inserted. Its style attribute will be updated with a height that
+                        // needs to be passed back up to the parent iframe, to avoid any
+                        // clipping. The height can change if the user is signed in with
+                        // many Google accounts, and clicks the 'X more accounts' button.
+                        const attributeObserver = new MutationObserver(function(iframeMutationsList) {
+                            const height = iframeMutationsList[0].target.style.height;
+                            messageParent({type: 'height', height});
+                        });
+                        attributeObserver.observe(node, { attributes: true });
+                    }
+                });
+            });
+        });
+        bodyObserver.observe(window.document.body, { childList: true });
     };
 };
